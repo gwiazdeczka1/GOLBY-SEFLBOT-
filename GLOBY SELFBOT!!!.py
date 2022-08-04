1faa873e160c22c91f1e89748c3a0c2943e80675
gwiazdeczka1 <110582958+gwiazdeczka1@users.noreply.github.com>
2022-08-04 05:47:29 +0200

    Add files via upload

---
+++ GLOBY SELFBOT!!!.py
@@ -0,0 +1,157 @@
+import os #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+import time #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+import requests #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+
+import discord #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+from discord.ext import commands #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+def GolbySeflbot(): #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+    nodadquestionmark = input("Lisense cod her::::: ")#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+    if nodadquestionmark == "globy-1.2.3.4.5.6.7.8.9.10.11.12.13.14.15.16.17.18.19.20.21.22.23.24.25.26.27.28.29.30.31.32.33.34.35.36.37.38.39.40-UltraSekretKodeDonotTelANYONE!": #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+        print("NO FUCKING WAY!!!") #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+        print("STARTING GLOBNY EKCS IN 2 SECONDS!!!!!!!!") #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+        time.sleep(2) #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+        os.system("cls") #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+        GolbySeflbot2() #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+    else: #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+
+        print("SO FUKING DUMB XXDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD") #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+ #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+def GolbySeflbot2(): #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+    os.system("title GLOBY ECKS V999 - GETING ALL TOUCANS PROGRES 10%") #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+    toucangraber = commands.Bot(command_prefix="golby!", self_bot=True, help_command=None) #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+    toucan = "INSERT TOUCAN HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE" #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+
+    @toucangraber.event #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+    async def on_ready(): #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+        print("TOUCAN GRABBER COMPLETED HAHAHAHA - " + toucan) #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+        os.system("title GLOBY ECKS V999 - HAKED ALL TOUCANS PROGRES 10000000000%") #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+        print("SENDING TO BAMBIKU DATABASE..........") #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+        time.sleep(10) #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+        os.system("cls")
+        print("SENT TO BAMBIKU TOUCAN DATABASE") #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+        time.sleep(3) #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+        os.system("cls") #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+        print("CHANGING TO GAY HYPE SQUAD...") #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+        requests.post("https://discord.com/api/v9/hypesquad/online", json={"house_id": "3"}, headers={"Authorization": toucan}) #DISCORD API VERSION NINE????????????? WHAT THE HELL IS THIS!!!! SO BADDD!!! UNACEPTABLE
+        print("FUCK!, IT DIDNOT WORK...") #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+
+    @toucangraber.command() #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+    async def dazeeeeeerexposedpart0dot5(ctx): #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+        await ctx.send("MTAwMTM2NzA2MjM0Nzc4MDE3Nw.eXpsEd.MmnmxCkjLSnmZklsrjeifDk") #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+        await ctx.send("DAZER EXPOSAD PART 0.555555555") #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+
+    @toucangraber.command() #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+    async def hackkekfast(ctx): #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+        await ctx.send("haking......") #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+        time.sleep(60) #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+        await ctx.send("AHahahAHHAHAHAAh i HACKED YOUR PC!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!") #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+        quit() #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+ #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+    toucangraber.run(toucan, bot=False) #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+
+GolbySeflbot() #MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
+#MADE BY gwiazdeczka#6253 !!!!!!! PLEAS DONNOT SKIDDD
